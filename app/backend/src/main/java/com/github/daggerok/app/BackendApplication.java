fc6c43f47fa88d3d423cb9d688af2848abdee547
Maksim Kostromin <daggerok@gmail.com>
2018-11-25 22:53:55 +0200

    Add hz.

--- app/backend/src/main/java/com/github/daggerok/app/BackendApplication.java
+++ app/backend/src/main/java/com/github/daggerok/app/BackendApplication.java
@@ -1,7 +1,27 @@
 package com.github.daggerok.app;
 
+import com.github.daggerok.library.HzCfgFactory;
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class HzCfg {
+
+  @Bean
+  Config hzConfig() {
+    return HzCfgFactory.hzCfg();
+  }
+
+  @Bean
+  HazelcastInstance hzInstance(Config hzConfig) {
+    return Hazelcast.newHazelcastInstance(hzConfig);
+  }
+}
 
 @SpringBootApplication
 public class BackendApplication {
